f2b15df6a51783f29aaa77770f934094d9533ff2
Amaro19 <fraa.guzman@duocuc.cl>
2026-09-02 15:57:16 -0400

    Agregar ms-pedidos

---
+++ ms-pedidos/src/test/java/cl/duoc/pedidos360/mspedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.pedidos360.mspedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
